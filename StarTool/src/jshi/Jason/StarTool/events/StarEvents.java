024fbc1492cba425743361276724b791f98bfb9e
unknown <jshigoodies@gmail.com>
2021-04-26 22:54:56 -0700

    spigot

---
+++ StarTool/src/jshi/Jason/StarTool/events/StarEvents.java
@@ -0,0 +1,14 @@
+package jshi.Jason.StarTool.events;
+
+import org.bukkit.event.Listener;
+
+import jshi.Jason.StarTool.Main;
+
+public class StarEvents implements Listener{
+	static Main plugin;
+	public StarEvents(Main instance) //constructor
+	{
+		plugin = instance;
+	}
+	//don't really need this, it's just a way to acess stuff from the main class. So I should make the outline like this from now on.
+}
