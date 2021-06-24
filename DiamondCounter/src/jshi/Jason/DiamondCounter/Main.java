bd78dcefe6d14b2cbc6f558b183e4eea756175fd
unknown <jshigoodies@gmail.com>
2021-06-23 22:08:16 -0700

    spigot

--- DiamondCounter/src/jshi/Jason/DiamondCounter/Main.java
+++ DiamondCounter/src/jshi/Jason/DiamondCounter/Main.java
@@ -8,18 +8,22 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import jshi.Jason.DiamondCounter.Commands.AmountDia;
 import jshi.Jason.DiamondCounter.Files.DataManager;
+import jshi.Jason.DiamondCounter.Files.DataManagerTwo;
 
 public class Main extends JavaPlugin implements Listener{
 	
 	public DataManager data;
+	public DataManagerTwo data_two;
 	
 	@Override
 	public void onEnable() {
-		this.data = new DataManager(this);
+		this.data = new DataManager(this); //diamonds
+		this.data_two = new DataManagerTwo(this); //death
 		getCommand("diamonds").setExecutor(new AmountDia(this, data));
 		this.getServer().getPluginManager().registerEvents(this, this);
 		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[DiamondCounter]: Plugin is Enabled");
@@ -54,5 +58,25 @@ public class Main extends JavaPlugin implements Listener{
 			data.getConfig().set("players." + player.getName(), (amount + 1));
 			data.saveConfig();
 		}
+	}
+	
+	@EventHandler
+	public void onEntityDeath(EntityDeathEvent event)
+	{
+		if(!(event.getEntity() instanceof Player))
+		{
+			return;
+		}
+		
+		Player player = (Player) event.getEntity();
+		player.sendMessage("you died");
+		int death = 0;
+		
+		if(this.data_two.getConfig().contains("death." + player.getName()));
+		{
+			death = this.data_two.getConfig().getInt("death." + player.getName());
+		}
+		data_two.getConfig().set("death." + player.getName(), (death + 1));
+		data_two.saveConfig();
 	}
 }
