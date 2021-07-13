d18324efe88e1ee85da7a7a60748232f73ab9817
unknown <jshigoodies@gmail.com>
2021-07-12 23:13:12 -0500

    spigot

--- DiscordSpigot/src/main/java/jshi/Jason/DiscordSpigot/Main.java
+++ DiscordSpigot/src/main/java/jshi/Jason/DiscordSpigot/Main.java
@@ -1,12 +1,5 @@
 package jshi.Jason.DiscordSpigot;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
-
-import javax.security.auth.login.LoginException;
-
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
@@ -21,33 +14,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 import jshi.Jason.DiscordSpigot.Commands.AmountDia;
 import jshi.Jason.DiscordSpigot.Files.DataManager;
 import jshi.Jason.DiscordSpigot.Files.DataManagerTwo;
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.OnlineStatus;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.TextChannel;
+import jshi.Jason.DiscordSpigot.bot.Bot;
 
 public class Main extends JavaPlugin implements Listener{
 	
 	public DataManager data;
-	public DataManagerTwo data_two;
-	
-	public static JDA jda;
-	public static String prefix = "~";
-	private Guild guild;
-	private List<TextChannel> listCh;
-	
+	public DataManagerTwo data_two;	
+	public Bot bot;
 	
 	@Override
 	public void onEnable() {
 		this.data = new DataManager(this); //diamonds
 		this.data_two = new DataManagerTwo(this); //death
 		
-		setUp();
+		bot = new Bot(this);
 		
 		getCommand("diamonds").setExecutor(new AmountDia(this, data));
 		this.getServer().getPluginManager().registerEvents(this, this);
-		listCh.get(0).sendMessage("plz work").queue();
 		getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[DiamondCounter]: Plugin is Enabled");
 	}
 
@@ -55,65 +38,7 @@ public class Main extends JavaPlugin implements Listener{
 	public void onDisable() {
 		getServer().getConsoleSender().sendMessage(ChatColor.RED + "[DiamondCounter]: Plugin is Disabled");
 	}
-	
-	public void setUp()
-	{
-		File f = new File("C:\\Users\\Jshi\\Desktop\\WorkSpace\\DiscordSpigot\\token.dat");
-		Scanner input = null;
-		try {
-			input = new Scanner(f);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		String token = input.nextLine();
-		JDABuilder building = JDABuilder.createDefault(token);
-		//building.addEventListeners(this);
-		try {
-			jda = building.build();
-		} catch (LoginException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//done building
-		
-		jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
-		
-		input.close();
-		
-		//organizing is good
-		additionalSetUp();
-	}
-	
-	private void additionalSetUp() //this adds the channels that the bot can send to
-	{
-		guild = jda.getGuildById("772158782758715403");
-		listCh = guild.getTextChannels();
-	}
-	
-	// Events
-//	@Override
-//	public void onReady(ReadyEvent event)
-//	{
-//		this.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[DiamondCounter] Bot setup success!");
-//		
-//		//server is up
-//		int index = -1;
-//		for(int i = 0; i<listCh.size(); i++)
-//		{
-//			if(listCh.get(i).getName().equalsIgnoreCase("general"))
-//			{
-//				index = i;
-//				break;
-//			}
-//		}
-//		
-//		if(index != -1)
-//		{
-//			listCh.get(index).sendMessage(":green_apple: Sever is up").queue();
-//		}
-//	}
+
 	
 	@EventHandler
 	public void onBlocBreak(BlockBreakEvent event)
---
+++ DiscordSpigot/src/main/java/jshi/Jason/DiscordSpigot/bot/Bot.java
@@ -0,0 +1,95 @@
+package jshi.Jason.DiscordSpigot.bot;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.security.auth.login.LoginException;
+
+import org.bukkit.ChatColor;
+
+import jshi.Jason.DiscordSpigot.Main;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Bot extends ListenerAdapter{
+	
+	private static JDA jda;
+	public static String prefix = "~";
+	private Main plugin;
+	private Guild guild;
+	private List<TextChannel> listCh;
+	
+	//i know this looks stupid, but I'm literally open to suggestions on how to make discord and minecraft work together
+	
+	public Bot(Main main)
+	{
+		plugin = main;
+		setUp(); 
+	}
+	
+	public void setUp()
+	{
+		File f = new File("token.dat");
+		Scanner input = null;
+		try {
+			input = new Scanner(f);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String token = input.nextLine();
+		JDABuilder building = JDABuilder.createDefault(token);
+		building.addEventListeners(this);
+		try {
+			jda = building.build();
+		} catch (LoginException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//done building
+		
+		jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
+		
+		input.close();
+		
+		//organizing is good
+		additionalSetUp();
+	}
+	
+	private void additionalSetUp() //this adds the channels that the bot can send to
+	{
+		guild = jda.getGuildById("772158782758715403");
+		listCh = guild.getTextChannels();
+	}
+	
+	// Events
+	@Override
+	public void onReady(ReadyEvent event)
+	{
+		plugin.getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[DiamondCounter] Bot setup success!");
+		
+		//server is up
+		int index = -1;
+		for(int i = 0; i<listCh.size(); i++)
+		{
+			if(listCh.get(i).getName().equalsIgnoreCase("general"))
+			{
+				index = i;
+				break;
+			}
+		}
+		
+		if(index != -1)
+		{
+			listCh.get(index).sendMessage(":green_apple: Sever is up").queue();
+		}
+	}
+}
